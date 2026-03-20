382e3c43e2b5ac38be295d8f97bfa0c31d85ccd2
shaonexplorer <shaonexplorer@gmail.com>
2026-03-20 21:10:25 +0600

    configuring cors

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // No trailing slash
+    origin: [
+      "http://localhost:5173",
+      "https://rebase-mern-challage-client.vercel.app",
+    ], // No trailing slash
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
